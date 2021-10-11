c24ef7dc163725eda01d87df20f6357abe1ed5fe
soft-dev <samir.beall@gmail.com>
2021-10-11 09:53:48 -0700

    added config files etc etc

---
+++ solenoid.py
@@ -0,0 +1,27 @@
+from pidev.Cyprus_Commands import Cyprus_Commands_RPi as cyprus
+from time import *
+
+
+cyprus.open_spi()
+
+
+def off():
+    cyprus.set_pwm_values(1, period_value=100000, compare_value=0,
+                          compare_mode=cyprus.LESS_THAN_OR_EQUAL)
+def idle():
+    cyprus.set_pwm_values(1, period_value=100000, compare_value=50000,
+                          compare_mode=cyprus.LESS_THAN_OR_EQUAL)
+
+def on():
+    cyprus.set_pwm_values(1, period_value=100000, compare_value=100000,
+                                  compare_mode=cyprus.LESS_THAN_OR_EQUAL)
+
+
+
+for i in range(10):
+    off()
+    sleep(1)
+    on()
+    sleep(.01)
+
+off()
