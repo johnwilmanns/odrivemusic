ea7f05fe2bd08ea296cf1a0145dc337c1d0483df
john wilmanns <johnwilmanns@dpengineering.org>
2021-10-13 13:30:09 -0700

    kjgtsghjkl

--- main.py
+++ main.py
@@ -3,36 +3,32 @@ import ODrive_Ease_Lib
 import time
 import serial
 import solenoid
+import math
 import odrive
 import usb.core
 from odrive.enums import *
 from pynput import keyboard
 
+solenoid.off()
 
 ax, od = ODrive_Ease_Lib.generic_startup()
 ax.scuffed_home()
+ax.set_pos(5.5)
+
 
 def hit(number):
     # print("hit " + number)
     pos = 5.5 + (number -1)
     ax.set_pos(pos)
+    while abs(pos - ax.get_pos()) > .1:
+        print(ax.get_vel())
+        time.sleep(.02)
     solenoid.on()
-    time.sleep(.01)
+    time.sleep(.05)
     solenoid.off()
 def on_press(key):
     print(type(key))
     hit(int(key.char))
-
-
-
-    #plz forgive me i know this is scuffed
-    # if key == "a":
-    #     hit(0)
-    # elif key == "s":
-    #     hit(1)
-    # elif key == "d":
-    #     hit(2)
-
 
 
 with keyboard.Listener(
--- solenoid.py
+++ solenoid.py
@@ -1,27 +1,42 @@
+
+import spidev
+import os
+from time import sleep
+import RPi.GPIO as GPIO
+
 from pidev.Cyprus_Commands import Cyprus_Commands_RPi as cyprus
-from time import *
 
+try:
+    cyprus.close_spi()
 
-cyprus.open_spi()
+except:
+    pass
 
-
+spi = spidev.SpiDev()
+cyprus.initialize()
 def off():
+    print("off")
     cyprus.set_pwm_values(1, period_value=100000, compare_value=0,
                           compare_mode=cyprus.LESS_THAN_OR_EQUAL)
 def idle():
+    print("idle")
     cyprus.set_pwm_values(1, period_value=100000, compare_value=50000,
                           compare_mode=cyprus.LESS_THAN_OR_EQUAL)
 
 def on():
+    print("on")
     cyprus.set_pwm_values(1, period_value=100000, compare_value=100000,
                                   compare_mode=cyprus.LESS_THAN_OR_EQUAL)
 
 
 if __name__ == "__main__":
-    for i in range(10):
+
+    for i in range(3):
         off()
         sleep(1)
         on()
-        sleep(.01)
+        sleep(1)
+        idle()
+        sleep(1)
 
     off()
